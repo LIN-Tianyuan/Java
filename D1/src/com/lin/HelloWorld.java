252347ab226a55982d19ebd773264f04ce936359
LIN-Tianyuan <sgsgkxkx@gmail.com>
2025-01-13 20:38:04 +0100

    D1

---
+++ D1/src/com/lin/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.lin;
+
+/**
+ * @author ：lin
+ * @date ：Created in 2025/1/13
+ * @description ：
+ * @version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
