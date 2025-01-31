fbaeb6f4bf96eef3aa4c44beb8a6cd66c9ea85bd
LIN-Tianyuan <sgsgkxkx@gmail.com>
2025-01-31 20:42:41 +0100

    Move pictures

--- puzzlegame/src/com/lin/ui/GameJFrame.java
+++ puzzlegame/src/com/lin/ui/GameJFrame.java
@@ -2,6 +2,8 @@ package com.lin.ui;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 /**
@@ -10,10 +12,12 @@ import java.util.Random;
  * @description ：Game
  * @version: 1.0
  */
-public class GameJFrame extends JFrame {
+public class GameJFrame extends JFrame implements KeyListener {
     // Create a two-dimensional array
     // Purpose: to manage data
     int[][] data = new int[4][4];
+    // Record the position of the blank squares in the two-dimensional array
+    int x = 0, y = 0;
     // Main screen of the game
     public GameJFrame() {
         // Initialization
@@ -48,13 +52,20 @@ public class GameJFrame extends JFrame {
 
         // 4. Add data to a two-dimensional array
         for (int i = 0; i < tempArr.length; i++) {
-            data[i/4][i%4] = tempArr[i];
+            if(tempArr[i] == 0) {
+                x = i / 4;
+                y = i % 4;
+            }else{
+                data[i/4][i%4] = tempArr[i];
+            }
         }
     }
 
     // Init image
     // When adding images, we need to add images according to the data managed in the two-dimensional array
     private void initImage() {
+        // Empty all images that were already there
+        this.getContentPane().removeAll();
         // Detail:
         // Images loaded first are at the top and images loaded later are at the bottom.
         // Outer loop: The inner loop is repeated 4 times
@@ -84,6 +95,9 @@ public class GameJFrame extends JFrame {
         background.setBounds(40, 40, 508, 560);
         // Add a background image to the interface
         this.getContentPane().add(background);
+
+        // Refresh screen
+        this.getContentPane().repaint();
 
     }
 
@@ -129,5 +143,65 @@ public class GameJFrame extends JFrame {
 
         // Uncheck the default centering, only then will the components be added according to the XY axis
         this.setLayout(null);
+        // Skype keyboard monitoring for the entire interface
+        this.addKeyListener(this);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Make judgments about up and down, left and right
+        // Left:37 Up:38 Right:39 Down:40
+        int code = e.getKeyCode();
+        if (code == 37) {
+            if (y == 3) {
+                return;
+            }
+            data[x][y] = data[x][y + 1];
+            data[x][y + 1] = 0;
+            y++;
+            // Call the method to load the image according to the latest number
+            initImage();
+        } else if (code == 38) {
+            if (x == 3) {
+                // Indicates that the blank square is already at the bottom, and that there is no picture below him that can be moved anymore
+                return;
+            }
+            // x, y: Blank
+            // x+1, y: Underneath the blank block
+            // Assign the number below the blank block to the blank square
+            data[x][y] = data[x + 1][y];
+            data[x + 1][y] = 0;
+            x++;
+            // Call the method to load the image according to the latest number
+            initImage();
+        } else if (code == 39) {
+            if (y == 0) {
+                return;
+            }
+            data[x][y] = data[x][y - 1];
+            data[x][y - 1] = 0;
+            y--;
+            // Call the method to load the image according to the latest number
+            initImage();
+        } else if (code == 40) {
+            if (x == 0) {
+                return;
+            }
+            data[x][y] = data[x - 1][y];
+            data[x - 1][y] = 0;
+            x--;
+            // Call the method to load the image according to the latest number
+            initImage();
+        }
     }
 }
